39836e31acc0c74062f2ba7eb3aa667530cdcc17
Guillermo Pincay <guillermaster@gmail.com>
2017-10-01 23:26:15 -0500

    xunit tests added

---
+++ Bookadoc.Tests.Unit/Data/EntityFramework/Repositories/UserRepositoryShould.cs
@@ -0,0 +1,45 @@
+﻿using Bookadoc.Core.Models;
+using Bookadoc.Data.EntityFramework;
+using Bookadoc.Data.EntityFramework.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Bookadoc.Tests.Unit.Data.EntityFramework.Repositories
+{
+    public class UserRepositoryShould
+    {
+        private readonly UserRepository _userRepository;
+
+        public UserRepositoryShould()
+        {
+            //Given
+            // https://docs.microsoft.com/en-us/ef/core/miscellaneous/testing/in-memory
+            var options = new DbContextOptionsBuilder<BookadocContext>()
+                .UseInMemoryDatabase(databaseName: "Bookadoc")
+                .Options;
+
+            using(var context = new BookadocContext(options))
+            {
+                context.Users.Add(new User { Name = "Guillermo", LastName = "Pincay", Email = "guillermaster@gmail.com" });
+                context.SaveChanges();
+            }
+
+            var bookadocContext = new BookadocContext(options);
+            _userRepository = new UserRepository(bookadocContext);
+        }
+
+        [Fact]
+        public async void ReturnGuillermoGivenIdOf1()
+        {
+            // When
+            var user = await _userRepository.Get(1);
+
+            // Then
+            Assert.NotNull(user);
+            Assert.Equal("R2-D2", user.Name);
+        }
+    }
+}
---
+++ Bookadoc.Tests.Unit/Data/InMemory/BookadocRepositoryShould.cs
@@ -0,0 +1,29 @@
+﻿using Bookadoc.Data.InMemory;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Bookadoc.Tests.Unit.Data.InMemory
+{
+    public class BookadocRepositoryShould
+    {
+        private readonly UserRepository _userRepository;
+
+        public BookadocRepositoryShould()
+        {
+            _userRepository = new UserRepository();
+        }
+
+        [Fact]
+        public async void ReturnGuillermoGivenIdOf1()
+        {
+            //When
+            var user = await _userRepository.Get(1);
+
+            //Then
+            Assert.NotNull(user);
+            Assert.Equal("Guillermo", user.Name);
+        }
+    }
+}
